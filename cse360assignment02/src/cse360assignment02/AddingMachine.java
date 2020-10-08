e60a5acc4d510c603b313114b4577ef54bdb3b7e
Cameron <Cameron@Camerons-Desktop>
2020-10-07 19:46:29 -0700

    Makes all functions work and adds main method for testing.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,42 @@ package cse360assignment02;
 
 public class AddingMachine {
 	  private int total;
+	  private String history;
 	  
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  public void add (int value) {
+		  total = total + value;
+		  history = history + " + " + value;
 	  }
 
 	  public void subtract (int value) {
+		  total = total - value;
+		  history = history + " - " + value;
 	  }
 
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 
 	  public void clear() {
+		  total = 0;
+		  history = "0";
 	  }
+	  
+	  public static void main(String[] args) {
+		    //Testing code
+			AddingMachine myCalculator = new AddingMachine();
+			myCalculator.add(4);
+			myCalculator.subtract(2);
+			myCalculator.add(5);
+			System.out.println(myCalculator.toString());
+		}
 	}
